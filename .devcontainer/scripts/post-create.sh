2ccd298a11d8dfe287d6a8d357adcc98136a3e1d
Tom <dev@tomblanc.fr>
2024-02-02 08:52:33 +0100

    Adding first init files

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,12 @@
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${CYAN}Runing post-create.sh${NC}"
+
+cd /workspace
+
+bash /workspace/.devcontainer/scripts/git/add-remote-templates.sh
+
+exit 0
---
+++ .devcontainer/scripts/post-start.sh
@@ -0,0 +1,29 @@
+cd /workspace/.devcontainer/scripts
+
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${CYAN}Starting post-start script...${NC}"
+
+# Fetch all branches from git
+git fetch --all
+
+# Pull current branch from git
+git pull
+
+# check if git template repo are up-to-date
+template_repo_status=$(bash ./git/check-merge-templates.sh)
+
+# Display hello message
+bash ./hello-message.sh
+
+if [[ $template_repo_status == *"not-up-to-date"* ]]; then
+    echo -e "${RED}Your template repo are not up-to-date!${NC}"
+    echo -e "${RED}Run ${CYAN}bash /workspace/.devcontainer/scripts/git/check-merge-templates.sh${RED} to check your template status${NC}"
+else
+    echo -e "${GREEN}Your template repo are up-to-date!${NC}"
+fi
+
+exit 0
